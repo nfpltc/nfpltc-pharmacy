73e7d300e9caf1b47bd2dbb0a04f2f1e9147fe22
nfpltc <northfalmouth6@gmail.com>
2026-04-03 00:27:25 -0400

    Update site-header.tsx

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -155,6 +155,7 @@ export function SiteHeader() {
                 ["Enrollment Form", "/forms/enrollment"],
                 ["Credit Card Update Form", "/forms/credit-card-update"],
                 ["Vaccine Consent Form", "/forms/vaccine-consent"],
+                ["My Statements", "/forms/statements"],
               ].map(([label, href]) => (
                 <Link
                   key={href}
@@ -250,6 +251,7 @@ export function SiteHeader() {
                 ["Vaccine Consent Form", "/forms/vaccine-consent"],
                 ["Enrollment Form", "/forms/enrollment"],
                 ["Credit Card Update Form", "/forms/credit-card-update"],
+                ["My Statements", "/forms/statements"],
               ].map(([label, href]) => (
                 <Link
                   key={href}
